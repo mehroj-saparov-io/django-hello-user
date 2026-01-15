9cd68745e0a205c2f9c3dc9216c8946697acdd8a
mehroj-saparov-io <saparov.dev2026@gmail.com>
2026-01-16 02:58:59 +0500

    init

---
+++ authentication/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'register.html', {
+                'error': 'Bu username allaqachon mavjud'
+            })
+
+        User.objects.create_user(
+            username=username,
+            password=password
+        )
+        return redirect('login')
+
+    return render(request, 'register.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {
+                'error': 'Username yoki parol xato'
+            })
+
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
